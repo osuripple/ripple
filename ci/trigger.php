6568d0374eedad0ee21305f1b5eb0dbbc30b9dbb
Nyo <to@nyo.zz.mu>
2016-01-12 17:43:35 +0100

    I hate this ci thing

--- ci/trigger.php
+++ ci/trigger.php
@@ -1,4 +1,4 @@
 <?php
 // THE SUPER RIPPLE CONTINUOUS INTEGRATION SYSTEM
 // It actually just calls the bash script at the bottom that updates everything locally.
-shell_exec("/usr/bin/bash " . dirname(__FILE__) . "/../ci-system/ci.sh /tmp/ci.log 2>>/tmp/ci.log &");
+shell_exec("/usr/bin/env bash " . dirname(__FILE__) . "/../ci-system/ci.sh /tmp/ci.log 2>>/tmp/ci.log &");
