06025f930be0f0d18fe0a1f43e4ba7c4fde3f2c4
Nyo <to@nyo.zz.mu>
2016-01-12 17:19:04 +0100

    Update .gitignore and ci

--- ci/trigger.php
+++ ci/trigger.php
@@ -1,5 +1,4 @@
 <?php
 // THE SUPER RIPPLE CONTINUOUS INTEGRATION SYSTEM
 // It actually just calls the bash script at the bottom that updates everything locally.
-shell_exec("/usr/bin/nohup " . dirname(__FILE__) . "/../ci-system/ci.sh > " . dirname(__FILE__) . "/../nohup.log 2>&1&");
-//shell_exec("/usr/bin/nohup " . dirname(__FILE__) . "/../ci-system/ci.sh > " . dirname(__FILE__) . "/../nohup.log 2>&1&");
+shell_exec("/usr/bin/bash " . dirname(__FILE__) . "/../ci-system/ci.sh /tmp/ci.log 2>>/tmp/ci.log &");
