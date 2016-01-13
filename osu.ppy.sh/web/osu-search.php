a1ced78f52ba68545001e27805486ef2c62bad3d
Nyo <to@nyo.zz.mu>
2016-01-13 16:59:48 +0100

    Add placeholder osu-search.php

---
+++ osu.ppy.sh/web/osu-search.php
@@ -0,0 +1,5 @@
+<?php
+	// bmapid.osz|Artist|Song name|mapper|ranked(1/0)|idk(prob star rating)|last update|bmap id again|topic id tho|has video(0/1)|0|0||Diff 1@mode,Diff 2@mode
+	echo("1
+96374.osz|Ripple|Osu! Direct hype!|FokaBot|3|10.00000|2016-01-04T16:10:16.000Z|96374|96374|1|0|1||Diff 1@0,Diff 2@1,Diff 3@2,Diff 4@3");
+?>
