9e8d82146d7e488417f99a9c1ff4481c0914b55d
Nyo <to@nyo.zz.mu>
2016-01-13 16:38:12 +0100

    Updated ci trigger again

--- ci/trigger.php
+++ ci/trigger.php
@@ -7,8 +7,7 @@ try
 	if (!isset($_POST["secret"]) || empty($_POST["secret"]) || $_POST["secret"] != SECRET)
 		throw new Exception("Invalid secret key");
 	
-	shell_exec("sh ../ci-system/ci.sh > /tmp/ci.log");
-	//shell_exec("/usr/bin/env bash " . dirname(__FILE__) . "/../ci-system/ci.sh /tmp/ci.log 2>>/tmp/ci.log &");
+	shell_exec("/usr/bin/env bash " . dirname(__FILE__) . "/../ci-system/ci.sh /tmp/ci.log 2>>/tmp/ci.log &");
 }
 catch (Exception $e)
 {
