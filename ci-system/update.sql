267069243b3eb6a58b057024cb65871d5d6b8303
Nyo <to@nyo.zz.mu>
2016-01-15 15:13:27 +0100

    .HIDE. Update update.sql

--- ci-system/update.sql
+++ ci-system/update.sql
@@ -1,3 +1,12 @@
 /* Update query goes here */
-INSERT INTO `bancho_settings` (`name`, `value_int`, `value_string`)
-VALUES ('login_notification', '0', '');
+-- Adminer 4.2.3 MySQL dump
+
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `when` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- 2016-01-15 14:13:01
