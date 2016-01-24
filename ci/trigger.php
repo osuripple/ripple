6e56890de462173d1e6df4a369004b305fc61525
Howl <git@howl.moe>
2016-01-24 22:46:58 +0100

    tell gogs whether it has begun or has silently ignored

--- ci/trigger.php
+++ ci/trigger.php
@@ -3,5 +3,8 @@
 // It actually just calls the bash script at the bottom that updates everything locally.
 $data = json_decode(file_get_contents('php://input'), true);
 if ($data["ref"] == "refs/heads/production") {
+	echo "doing";
 	shell_exec("/usr/bin/env bash " . dirname(__FILE__) . "/../ci-system/ci.sh /tmp/ci.log 2>>/tmp/ci.log &");
+} else {
+	echo "ignored";
 }
