9c7d295eaf337c0119eea4f33b2c657aaaabc195
Howl <git@howl.moe>
2016-01-24 22:45:27 +0100

    Trigger CI script only if we've pushed on the prod branch

--- ci/trigger.php
+++ ci/trigger.php
@@ -1,4 +1,7 @@
 <?php
 // THE SUPER RIPPLE CONTINUOUS INTEGRATION SYSTEM
 // It actually just calls the bash script at the bottom that updates everything locally.
-shell_exec("/usr/bin/env bash " . dirname(__FILE__) . "/../ci-system/ci.sh /tmp/ci.log 2>>/tmp/ci.log &");
+$data = json_decode(file_get_contents('php://input'), true);
+if ($data["ref"] == "refs/heads/production") {
+	shell_exec("/usr/bin/env bash " . dirname(__FILE__) . "/../ci-system/ci.sh /tmp/ci.log 2>>/tmp/ci.log &");
+}
