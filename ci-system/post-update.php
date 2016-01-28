625d18680f02da8f471b37e998ef9c126c849b04
Nyo <to@nyo.zz.mu>
2016-01-28 21:07:54 +0100

    .HIDE. Reset post-update.php'

--- ci-system/post-update.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once(dirname(__FILE__) . "/../osu.ppy.sh/inc/functions.php");
-$messages = $GLOBALS["db"]->fetchAll("SELECT * FROM bancho_messages");
-foreach ($messages as $message)
-	$GLOBALS["db"]->execute("UPDATE bancho_messages SET msg_from_username = ? WHERE id = ?", array(getUserUsername($message["msg_from_userid"]), $message["id"]));
