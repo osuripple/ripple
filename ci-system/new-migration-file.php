ea2dc0e500d858e72bd35df55451f08167d49152
Howl <git@howl.moe>
2016-02-05 22:33:26 +0100

    .HIDE. fix unexpectederino trollerino

--- ci-system/new-migration-file.php
+++ ci-system/new-migration-file.php
@@ -1,5 +1,7 @@
 <?php
 $d = file_get_contents(dirname(__FILE__) . "/../migrations/latest.txt");
-$d++;
+do {
+    $d++;
+} while (file_exists(dirname(__FILE__) . "/../migrations/$d.php"));
 file_put_contents(dirname(__FILE__) . "/../migrations/$d.php", "<?php
 // Content goes here...");
