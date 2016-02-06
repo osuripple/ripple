b946a59b5a5a9049a19d0c659e9e3ae8826e2e0c
Nyo <to@nyo.zz.mu>
2016-02-06 15:57:22 +0100

    .HIDE. fml

---
+++ migrations/2.php
@@ -0,0 +1,25 @@
+<?php
+include dirname(__FILE__) . "/../osu.ppy.sh/inc/functions.php";
+
+$GLOBALS["db"]->execute("
+CREATE TABLE `bancho_private_messages` (
+  `id` int(11) NOT NULL,
+  `msg_from_userid` int(11) NOT NULL,
+  `msg_from_username` varchar(30) NOT NULL,
+  `msg_to` varchar(32) NOT NULL,
+  `msg` varchar(127) NOT NULL,
+  `time` int(11) NOT NULL
+);
+");
+
+$GLOBALS["db"]->execute("
+ALTER TABLE `bancho_private_messages` ADD PRIMARY KEY (`id`);
+");
+
+$GLOBALS["db"]->execute("
+ALTER TABLE `bancho_tokens` ADD `latest_private_message_id` INT NOT NULL AFTER `latest_message_id`;
+");
+
+$GLOBALS["db"]->execute("
+ALTER TABLE `bancho_private_messages` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT;
+");
