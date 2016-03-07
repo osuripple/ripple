9d23a694573a8af802439bbcf3beb38d4da45cab
Nyo <to@nyo.zz.mu>
2016-03-07 13:06:29 +0100

    .HIDE. Fixed ci script

--- ci-system/ci.sh
+++ ci-system/ci.sh
@@ -4,7 +4,8 @@
 tmux send -t avatarserver C-c
 
 # Trigger bancho ci event (safe shutdown)
-curl 127.0.0.1:5001/ci-trigger?k=$(< cikey.txt)
+cikey=`cat cikey.txt`
+curl 127.0.0.1:5001/ci-trigger?k=$cikey
 
 # Wait until bancho has been killed (25 seconds, 5 seconds of margin)
 sleep 30
