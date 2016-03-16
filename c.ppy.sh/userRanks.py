d8fd701f3b47b2467974ef4f6b858ab6f3925040
Avail <kanade7331@gmail.com>
2016-03-16 12:34:55 +0100

    .HIDE. usernameColors -> userRanks, updated data. Fixes #70

---
+++ c.ppy.sh/userRanks.py
@@ -0,0 +1,8 @@
+"""Bancho user ranks"""
+normal			= 0
+players			= 1
+supporter		= 4
+peppy			= 8
+mod				= 6
+admin			= 16
+tournamentstaff = 32
--- c.ppy.sh/usernameColors.py
+++
@@ -1,5 +0,0 @@
-"""Bancho username colors"""
-normal 		= 0
-supporter 	= 4
-mod			= 6
-admin		= 16
