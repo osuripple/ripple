9dd1c547da5a9952a8e62c0aa994fae1c193927f
0x617661696c <avail@users.noreply.github.com>
2016-03-26 11:56:52 +0100

    .HIDE. Auto create avatars directory. (Fix #74)

--- a.ppy.sh/avatarserver.py
+++ a.ppy.sh/avatarserver.py
@@ -3,16 +3,22 @@ import os
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 
+avatar_dir = "avatars" # no slash
+
+# create avatars directory if it does not exist
+if not os.path.exists(avatar_dir):
+    os.makedirs(avatar_dir)
+    
 @app.route("/<int:osuid>")
 def serveAvatar(osuid):
 	# Check if avatar exists
-	if os.path.isfile("avatars/%d.png" % osuid):
+	if os.path.isfile("{}/{}.png".format(avatar_dir, osuid)):
 		avatarid = osuid
 	else:
 		avatarid = 0
 
 	# Serve actual avatar or default one
-	return send_file("avatars/%d.png" % avatarid)
+	return send_file("{}/{}.png".format(avatar_dir, avatarid))
 
 # Run the server
 app.run(host="0.0.0.0", port=5000)
