b1b3b48eab8800d689a5950e0b3fd15dc5e7296f
Howl <git@howl.moe>
2016-03-28 14:47:13 +0200

    Revert ".hide. make ci more modular"
    This reverts commit 375697dca15257b537f2fe7be2751f45d553a0c8.

--- ci-system/ci-composer.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Run composer in various directories.
-originaldir=$(pwd)
-for i in 'osu.ppy.sh/blog'; do
-	cd $i
-	composer install
-	cd $originaldir
-done
--- ci-system/ci-stop.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Stop avatar server (with basic ctrl+c)
-tmux send -t avatarserver C-c
-
-# Trigger bancho ci event (safe shutdown)
-cikey=`cat cikey.txt`
-curl 127.0.0.1:5001/ci-trigger?k=$cikey
