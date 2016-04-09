1028e360ed4fc4f0ff5a2cc70c2916fbc4bc69f6
Howl <the@howl.moe>
2016-04-09 20:42:40 +0200

    .HIDE. Add API `tokens` table

---
+++ migrations/7.php
@@ -0,0 +1,9 @@
+<?php
+$GLOBALS["db"]->execute("CREATE TABLE `tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(31) NOT NULL,
+  `privileges` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `token` varchar(127) NOT NULL,
+  PRIMARY KEY (`id`)
+)");
