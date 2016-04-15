8ff759cf26c8da8c0f1a1816c6128a8e2367fdd7
Howl <the@howl.moe>
2016-04-15 21:19:40 +0200

    .HIDE. "Fix" fixindentation.sh not working

--- fixindentation.sh
+++ fixindentation.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # Script to fix borken indentation.
-find ./osu.ppy.sh -type f -iname "*.php" -not -path "./osu.ppy.sh/vendor/*" -print0 | while IFS= read -r -d $'\0' line; do echo "$line"; php_beautifier -t1 $line $line.out; mv $line.out $line; echo "   ... ok"; done
+# NEVERMIND NEVER EVER RUN THIS CURSED SCRIPT AGAIN IT WILL DESTROY THE CODE
+#find ./osu.ppy.sh -type f -iname "*.php" -not -path "./osu.ppy.sh/vendor/*" -print0 | while IFS= read -r -d $'\0' line; do echo "$line"; php_beautifier -t1 $line $line.out; mv $line.out $line; echo "   ... ok"; done
